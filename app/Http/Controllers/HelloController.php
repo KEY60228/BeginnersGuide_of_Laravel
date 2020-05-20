d07ad2408382e698ee6e92d9d1d47895a89169bd
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-21 06:45:00 +0900

    customized Validator's error messages

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,23 +10,33 @@ use Validator;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            $msg = 'something wrong in query.';
-        } else {
-            $msg = 'Thank you, we got your ID and pass.';
-        }
-
         return view('hello.index', [
-            'msg' => $msg,
+            'msg' => 'Input below forms.',
         ]);
     }
 
     public function post(Request $request) {
+        $rules = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric | between:0,150',
+        ];
         
+        $messages = [
+            'name.required' => '氏名を入力してください',
+            'mail.email' => 'メールアドレスを入力してください',
+            'age.numeric' => '年齢を整数で入力してください',
+            'age.between' => '年齢は0〜150の間で入力してください',
+        ];
+        
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
+        
+        return view('hello.index', [
+            'msg' => 'Thanks!',
+        ]);
     }
 }
