d17c78d8ada390e78861f5715b02ef736ec096ee
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-21 05:42:03 +0900

    Validation tutorial

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,24 @@
 @endsection
 
 @section('content')
-  <p>ここが本文のコンテンツ</p>
-  <p>This is link for <middleware>google.com</middleware></p>
-  <p>This is link for <middleware>yahoo.co.jp</middleware></p>
+  <p>{{ $msg }}</p>
+  <form action="/hello" method="post">
+    <table>
+      @csrf
+      <tr>
+        <th>name: </th><td><input type="text" name="name"></td>
+      </tr>
+      <tr>
+        <th>mail: </th><td><input type="text" name="mail"></td>
+      </tr>
+      <tr>
+        <th>age: </th><td><input type="text" name="age"></td>
+      </tr>
+      <tr>
+        <th></th><td><input type="submit" value="send"></td>
+      </tr>
+    </table>
+  </form>
 @endsection
 
 
