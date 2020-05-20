5ee5b8f39c7333db0809dbc0dd2f76eb1a9c5d65
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-21 06:36:22 +0900

    Introduce Validator for query

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,24 +10,23 @@ use Validator;
 class HelloController extends Controller
 {
     public function index(Request $request) {
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $msg = 'something wrong in query.';
+        } else {
+            $msg = 'Thank you, we got your ID and pass.';
+        }
+
         return view('hello.index', [
-            'msg' => 'input below forms.'
+            'msg' => $msg,
         ]);
     }
 
     public function post(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric | between:0,150',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/hello')->withErrors($validator)->withInput();
-        }
-
-        return view('hello.index', [
-            'msg' => 'Thanks!!',
-        ]);
+        
     }
 }
