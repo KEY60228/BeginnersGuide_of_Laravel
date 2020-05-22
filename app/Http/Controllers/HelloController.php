f59aee62116383d907759196075b40377ec3f61d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-22 22:23:08 +0900

    Introduce DB::insert

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,13 +11,40 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::select('select * from people');
+        if (isset($request->id)) {
+            $param = [
+                'id' => $request->id
+            ];
+            $items = DB::select('SELECT * FROM people WHERE id = :id', $param);
+        } else {
+            $items = DB::select('SELECT * FROM people');
+        }
+   
         return view('hello.index', [
             'items' => $items,
         ]);
     }
 
     public function post(Request $request) {
-        
+        $items = DB::select('SELECT * FROM people');
+        return view('hello.index', [
+            'items' => $items,
+        ]);
+    }
+
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+
+    public function create(Request $request) {
+        $params = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ]; 
+
+        DB::insert('INSERT INTO people (name, mail, age) VALUES (:name, :mail, :age)', $params);
+
+        return redirect('/hello');
     }
 }
